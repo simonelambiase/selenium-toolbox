9a0eb8ac5b3beea8812aec942d2dcd1410f7b293
slambiase <sbranino2@gmail.com>
2021-11-02 22:45:51 +0100

    Demo classes code refactoring

--- src/main/java/demo/Runner.java
+++ src/main/java/demo/Runner.java
@@ -1,10 +1,8 @@
 package demo;
 
+import demo.entities.WebPage;
 import persistence.PersistenceManager;
-import persistence.mysql.PersistenceManagerMariaDB;
 import persistence.mysql.PersistenceManagerSQLite;
-
-import javax.persistence.*;
 
 public class Runner {
 
@@ -12,69 +10,7 @@ public class Runner {
         PersistenceManager sql = new PersistenceManagerSQLite("resources/test");
         sql.connect();
         sql.saveObject(new WebPage("Amazon","www.amazon.it"));
-        System.out.println(sql.executeQuery("FROM demo.WebPage"));
+        System.out.println(sql.executeQuery("FROM demo.entities.WebPage"));
     }
 }
 
-@Entity
-@Table ( name = "web_page")
-class WebPage {
-
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY )
-    private int id;
-
-    @Column ( name = "page_name")
-    private String pageName;
-
-    @Column ( name = "page_url")
-    private String pageUrl;
-
-    public WebPage() {
-
-    }
-
-    public WebPage(int id, String pageName, String pageUrl) {
-        this.id = id;
-        this.pageName = pageName;
-        this.pageUrl = pageUrl;
-    }
-
-    public WebPage(String pageName, String pageUrl) {
-        this.pageName = pageName;
-        this.pageUrl = pageUrl;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPageName() {
-        return pageName;
-    }
-
-    public void setPageName(String pageName) {
-        this.pageName = pageName;
-    }
-
-    public String getPageUrl() {
-        return pageUrl;
-    }
-
-    public void setPageUrl(String pageUrl) {
-        this.pageUrl = pageUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "WebPage{" +
-                "id=" + id +
-                ", pageName='" + pageName + '\'' +
-                ", pageUrl='" + pageUrl + '\'' +
-                '}';
-    }
-}
